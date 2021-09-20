af0eb995b50e8b32bfb89fe875812afe2651aa64
Pratyaksha Gupta <82153437+Pratyaksha-047@users.noreply.github.com>
2021-09-20 18:05:15 +0530

    add

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ app= Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/instructions.html')
-def instructions():
-    return render_template('instructions.html')
+    return("Hello world")
 
 if __name__ =="__main__":
     app.run(debug=True)
