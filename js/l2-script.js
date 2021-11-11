c54df65cc46dcd96e581b33bd7547553c62a3951
jasleen1602 <84179720+jasleen1602@users.noreply.github.com>
2021-11-11 23:26:36 +0530

    Update l2-script.js

--- js/l2-script.js
+++ js/l2-script.js
@@ -203,7 +203,7 @@ function startTimer(time){
 }
 
 function startTimerLine(time){
-    counterLine = setInterval(timer, 320);
+    counterLine = setInterval(timer, 324);
     function timer(){
         time += 1; //upgrading time value with 1
         time_line.style.width = time + "px"; //increasing width of time_line with px by time value
